bfb4cc1c668669ca2afd7ebbc03d187ef315a231
b3n10 <francisben.lleve@gmail.com>
2020-04-14 20:47:06 +0800

    Update docker script

--- create_docker.sh
+++ create_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker run -itd $PWD:/var/www/html --link mysqlserver:mysqldb --name fpm php:7.4-fpm
 docker run -itd -p 1235:80 -v $PWD:/var/www/html --link mysqlserver:mysqldb --link fpm --name nginx nginx
